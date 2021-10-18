0ea5f352741af569f70eff7e57705aaf3b6a2eb8
Qnedy <altmaisefequatro@gmail.com>
2021-10-18 20:49:29 -0300

    added main colors, light theme colors and input icons

--- src/styles/themes/light.js
+++ src/styles/themes/light.js
@@ -2,10 +2,10 @@ export default{
   title: 'light',
 
   backgroundColors: {
-    primary: '',
-    secondary: '',
-    third: '',
-    fourth: '',
+    primary: '#1A4F8B',
+    secondary: '#385C8E',
+    third: '#F14436',
+    fourth: '#BF1541',
     fifth: '',
     sixth: '',
     seventh: '',
@@ -14,13 +14,13 @@ export default{
   },
 
   textColors: {
-    primary: '',
-    secondary: '',
-    third: '',
-    fourth: '',
-    fifth: '',
-    sixth: '',
-    seventh: '',
+    primary: '#000000',
+    secondary: '#1A4F8B',
+    third: '#767676',
+    fourth: '#C3C3C3',
+    fifth: '#AEAEAE',
+    sixth: '#B0B0B0',
+    seventh: '#fff',
     eighth: '',
     ninth: '',
   }
